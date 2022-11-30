d9e03f6ea3dddd68c0f80ec67e68fe02574a6370
ravinder.kumar1 <ravinder.kumar@researchandranking.com>
2022-11-30 12:00:18 +0530

    first commit

---
+++ portfoliobuilder.py
@@ -0,0 +1,56 @@
+import click
+
+@click.group()
+def portfoliobuilder():
+    pass
+
+
+###############################################
+"""
+CREATE GROUP
+"""
+
+
+@click.group(name='create')
+def create_group():
+      pass
+
+@click.command(name='create_customized_portfolio')
+def create_customized_portfolio():
+      pass
+
+
+##################################################
+"""
+FETCH GROUP
+"""
+
+
+
+@click.group(name='get')
+def get_group():
+      pass
+
+@click.command(name='daily_model_portfolio')
+def get_daily_model_poretfolio():
+      pass
+
+@click.command(name='view_breached_stocks')
+def get_breched_target_range_stocks():
+      pass
+
+@click.command(name="get_price")
+def get_stock_price():
+    """
+    to get stock price
+    :return:
+    """
+    pass
+
+
+
+create_group.add_command(create_customized_portfolio)
+get_group.add_command(get_daily_model_poretfolio)
+get_group.add_command(get_stock_price)
+portfoliobuilder.add_command(get_group)
+portfoliobuilder.add_command(create_group)
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup,find_packages,version
+
+
+setup(
+    name='portfoliobuilder',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        'aiohttp',
+        'requests'
+    ],
+    version='0.0.0.0',
+    entry_points='''    
+    [console_scripts]
+    portfolio_builder_cli=portfoliobuilder:portfoliobuilder
+    '''
+
+)

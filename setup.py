5a61232422adfa3b949ea57bc2c6762b8cb5e06b
ravinder.kumar1 <ravinder.kumar@researchandranking.com>
2022-11-30 15:30:56 +0530

    pypi not contians project files

--- setup.py
+++ setup.py
@@ -4,14 +4,17 @@ from setuptools import setup,find_packages,version
 setup(
     name='portfolio_builder_cli',
     packages=find_packages(),
+    email="ravinder.kumar@researchandranking.com",
+    author="ravinder",
     install_requires=[
         'click',
         'aiohttp',
         'requests'
     ],
 
-    version='0.0.0',
+    version='0.0.1',
     long_description_content_type="text/markdown",
+    data_files=[('portfoliobuilder.py')],
     entry_points='''    
     [console_scripts]
     portfoliobuilder=portfoliobuilder:portfoliobuilder
